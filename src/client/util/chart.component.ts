b7bd4514454fee57dbd2e6e6095c4f9f0c840833
Andreea Dumitru <dumitru_andreea96@yahoo.com>
2017-08-11 11:44:45 +0300

    chart component

---
+++ src/client/util/chart.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'chart.html',
+    selector: 'chart'
+})
+export class ChartComponent {}
