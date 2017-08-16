8c3a7f2cd23045e05bd9ea9c06184c865b363d19
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-16 15:30:48 +0300

    Using bar chart template

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<line-chart></line-chart>'
-  // template: '<bar-chart></bar-chart>'  
+  // template: '<line-chart></line-chart>'
+  template: '<bar-chart></bar-chart>'
   // template: '<pie-chart></pie-chart>'
 })
 export class AppComponent { }
