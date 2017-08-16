86f2a9f0507813e176af75ae83f5891cfec10c87
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-16 16:04:24 +0300

    Using bar data interface

--- src/server/controllers/barData.controller.ts
+++ src/server/controllers/barData.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { BarDataModel as Data } from '../models/barData';
-
+import { IBarData } from '../types/IBarData';
 export class DataController {
     constructor() { }
-    async getData(): Promise<Object[]> {
-        return await Data.find().lean().exec() as Object[];
+    async getData(): Promise<IBarData[]> {
+        return await Data.find().lean().exec() as IBarData[];
     }
-    async postData(newData): Promise<void> {
+    async postData(newData: IBarData): Promise<void> {
         let data = new Data(newData);
         await data.save();
     }
