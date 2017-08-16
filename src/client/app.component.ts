974759a12074cdd1138a053cba5bd02344fdd78e
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-16 14:05:34 +0300

    Removed unused imports and template elements

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import {ChartComponent } from './util/chart.component';
-import { LineChartComponent } from './lineChart.component';
 
 @Component({
-  
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <chart> </chart>
-    <pie-chart> </pie-chart>
-  `,
+  template: '<line-chart></line-chart>'
+  // template: '<bar-chart></bar-chart>'  
+  // template: '<pie-chart></pie-chart>'
 })
-export class AppComponent {
-}
+export class AppComponent { }
