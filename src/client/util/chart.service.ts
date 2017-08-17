91bb271e46826fa895f0ad9490af18ce11c0872c
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-17 14:51:53 +0300

    Removed data files

--- src/client/util/chart.service.ts
+++ src/client/util/chart.service.ts
@@ -1,29 +1,8 @@
 import { Component, Injectable } from '@angular/core';
-import { ChartComponent } from './chart.component';
 import { Http } from '@angular/http';
-import { Datum } from '../data';
-
-import 'rxjs/add/operator/toPromise';
+import { ChartComponent } from './chart.component';
 
 @Injectable()
 export class ChartService {
-    private headers = new Headers({'Content-Type': 'application/json'});
-
-    private url = 'http://localhost:3000/api/data';
     constructor(private http: Http) { }
-
-
-    getData(): Promise<Datum[]> {
-        return this.http.get(this.url)
-            .toPromise()
-            .then(response => response.json() as Datum[])
-            .catch(this.handleError);
-    }
-
-    private handleError(error) {
-        console.error('An error occurred', error);
-        return Promise.reject(error.message || error);
-    }
-
-
 }
