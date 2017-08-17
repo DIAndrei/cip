1d6b329541d55be65f2de26aaa4bbfe550144b0c
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-17 10:37:03 +0300

    Returning data from the server sorted ascending

--- src/server/controllers/lineData.controller.ts
+++ src/server/controllers/lineData.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { DataModel as Data } from '../models/lineData';
+import { ILineData } from '../types/ILineData';
 
 export class DataController {
     constructor() { }
-    async getData(): Promise<Object[]> {
-        return await Data.find().lean().exec() as Object[];
+    async getData(): Promise<ILineData[]> {
+        return await Data.find().sort({ 'date': 1 }).lean().exec() as ILineData[];
     }
     async postData(newData): Promise<void> {
         let data = new Data(newData);
