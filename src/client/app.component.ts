35de040c2cd96802e48aaf138486c4b406db85b3
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-18 17:20:39 +0300

    Removed inline templates. Using different template file

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BarChartComponent } from './barChart/barChart.component';
+import { LineChartComponent } from './lineChart/lineChart.component';
+import { PieChartComponent } from './pieChart/pieChart.component';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: '<line-chart></line-chart>'
-  // template: '<bar-chart></bar-chart>'
-  // template: '<pie-chart></pie-chart>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent { }
