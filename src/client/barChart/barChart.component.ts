fa4d0b975a80d170fb3b2bcae8c1a1139a7b8933
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-21 10:45:20 +0300

    Removed unnecessary text from bar chart axis.

--- src/client/barChart/barChart.component.ts
+++ src/client/barChart/barChart.component.ts
@@ -71,8 +71,7 @@ export class BarChartComponent implements OnInit {
             .attr("transform", "rotate(-90)")
             .attr("y", 6)
             .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Frequency");
+            .attr("text-anchor", "end");
     }
 
     drawBars() {
