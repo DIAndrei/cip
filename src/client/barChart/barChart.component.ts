0f8cae3196edb0e49e933d1cd99412c00057d71e
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-22 15:10:41 +0300

    Implemented tooltip that shows on hover.

--- src/client/barChart/barChart.component.ts
+++ src/client/barChart/barChart.component.ts
@@ -10,16 +10,16 @@ import { IChartData } from './../../server/types/IChartData';
 })
 export class BarChartComponent implements OnInit {
     // @Input() data: IChartData[];
+    private data: IChartData[];
+
     private width: number;
     private height: number;
     private margin = { top: 20, right: 20, bottom: 30, left: 40 };
-
-    private data: IChartData[];
-
     private x: any;
     private y: any;
     private svg: any;
     private g: any;
+    private tooltip: any;
 
     constructor(
         private _barChartService: ChartService,
@@ -44,44 +44,51 @@ export class BarChartComponent implements OnInit {
     }
 
     initSvg() {
-        this.svg = d3.select("svg");
-        this.width = +this.svg.attr("width") - this.margin.left - this.margin.right;
-        this.height = +this.svg.attr("height") - this.margin.top - this.margin.bottom;
-        this.g = this.svg.append("g")
-            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
+        this.svg = d3.select('svg');
+        this.tooltip = d3.select('ng-component').append('div').attr('class', 'tooltip');
+        this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+        this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+        this.g = this.svg.append('g')
+            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');;
     }
 
     initAxis() {
-        this.x = d3.scaleBand().rangeRound([0, this.width]).padding(0.1);
-        this.y = d3.scaleLinear().rangeRound([this.height, 0]);
-        this.x.domain(this.data.map((d) => d.prop));
-        this.y.domain([0, d3.max(this.data, (d) => d.value)]);
+        this.x = d3.scaleLinear().rangeRound([0, this.width]);
+        this.y = d3.scaleBand().rangeRound([this.height, 0]).padding(0.1);
+        this.x.domain([0, d3.max(this.data, (d) => d.value)]);
+        this.y.domain(this.data.map((d) => d.prop));
     }
 
     drawAxis() {
-        this.g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + this.height + ")")
+        this.g.append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', 'translate(0,' + this.height + ')')
             .call(d3.axisBottom(this.x));
-        this.g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(this.y).ticks(10))
-            .append("text")
-            .attr("class", "axis-title")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end");
+
+        this.g.append('g')
+            .attr('class', 'axis axis--y')
+            .call(d3.axisLeft(this.y));
     }
 
     drawBars() {
-        this.g.selectAll(".bar")
+        this.g.selectAll('.bar')
             .data(this.data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", (d) => this.x(d.prop))
-            .attr("y", (d) => this.y(d.value))
-            .attr("width", this.x.bandwidth())
-            .attr("height", (d) => this.height - this.y(d.value));
+            .enter()
+            .append('rect')
+            .attr('class', 'bar')
+            .attr('x', 0)
+            .attr('y', (d) => this.y(d.prop))
+            .attr('height', this.y.bandwidth())
+            .attr('width', (d) => this.x(d.value))            
+            .on('mousemove', (d) => {
+                this.tooltip
+                    .style('left', d3.event.pageX - 50 + 'px')
+                    .style('top', d3.event.pageY - 70 + 'px')
+                    .style('display', 'inline-block')
+                    .html((d.prop) + ': ' + (d.value));
+            })
+            .on('mouseout', (d) => {
+                this.tooltip.style('display', 'none');
+            });
     }
 }
--- src/client/pieChart/pieChart.component.ts
+++ src/client/pieChart/pieChart.component.ts
@@ -12,6 +12,7 @@ export class PieChartComponent implements OnInit {
 
   // @Input() data: IChartData[];
   private data: IChartData[];
+
   private margin = { top: 20, right: 20, bottom: 30, left: 40 };
   private width: number;
   private height: number;
@@ -21,7 +22,7 @@ export class PieChartComponent implements OnInit {
   private pie: any;
   private color: any;
   private svg: any;
-
+  private tooltip: any;
 
   constructor(
     private _barChartService: ChartService,
@@ -49,6 +50,7 @@ export class PieChartComponent implements OnInit {
 
   private initSvg() {
     this.color = d3.scaleOrdinal(d3.schemeCategory10);
+    this.tooltip = d3.select('ng-component').append('div').attr('class', 'tooltip');
     this.arc = d3.arc()
       .outerRadius(this.radius - 10)
       .innerRadius(0);
@@ -58,21 +60,30 @@ export class PieChartComponent implements OnInit {
     this.pie = d3.pie()
       .sort(null)
       .value((d: any) => d.value);
-    this.svg = d3.select("svg")
-      .append("g")
-      .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");;
+    this.svg = d3.select('svg')
+      .append('g')
+      .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');;
   }
 
   private drawPie() {
-    let g = this.svg.selectAll(".arc")
+    let g = this.svg.selectAll('.arc')
       .data(this.pie(this.data))
-      .enter().append("g")
-      .attr("class", "arc");
-    g.append("path").attr("d", this.arc)
-      .style("fill", (d: any) => this.color(d.data.prop));
-    g.append("text").attr("transform", (d: any) => "translate(" + this.labelArc.centroid(d) + ")")
-      .attr("dy", ".35em")
+      .enter().append('g')
+      .attr('class', 'arc')
+      .on('mousemove', (d) => {
+        this.tooltip
+          .style('left', d3.event.pageX - 50 + 'px')
+          .style('top', d3.event.pageY - 70 + 'px')
+          .style('display', 'inline-block')
+          .html((d.data.prop) + ': ' + (d.data.value));
+      })
+      .on('mouseout', (d) => {
+        this.tooltip.style('display', 'none');
+      });
+    g.append('path').attr('d', this.arc)
+      .style('fill', (d: any) => this.color(d.data.prop));
+    g.append('text').attr('transform', (d: any) => 'translate(' + this.labelArc.centroid(d) + ')')
+      .attr('dy', '.35em')
       .text((d: any) => d.data.prop);
   }
-
 }
