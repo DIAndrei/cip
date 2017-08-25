434e95b7111f72e56fd449eac2b165d25d61609f
Andreea Dumitru <dumitru_andreea96@yahoo.com>
2017-08-25 14:02:22 +0300

    installs component report

---
+++ src/client/reports/installs/installs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'installs-comp',
+    templateUrl: './installs.component.html',
+    providers: []
+})
+
+export class InstallsComponent {
+    constructor(){}
+    private report: string = 'installs';
+
+}
