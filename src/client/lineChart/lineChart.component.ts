6926d9dcc9bc17607245756176921699b3907517
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-25 11:11:38 +0300

    Showing date on tooltip

--- src/client/lineChart/lineChart.component.ts
+++ src/client/lineChart/lineChart.component.ts
@@ -129,7 +129,7 @@ export class LineChartComponent implements IChart {
           .style('left', d3.event.pageX - 50 + 'px')
           .style('top', d3.event.pageY - 70 + 'px')
           .style('display', 'inline-block')
-          .html(((d.prop)) + ': ' + (d.sessions));
+          .html((d.date.toDateString()) + '<br>' + ((d.prop)) + ': ' + (d.sessions));
       })
       .on('mouseout', (d: any) => {
         this.tooltip.style('display', 'none');
