3781051dfd709597e8e4ec67668b5ce89ff99f66
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-25 12:37:29 +0300

    Moved component imports from app module to routing module

--- src/client/app-routing.module.ts
+++ src/client/app-routing.module.ts
@@ -3,9 +3,16 @@ import { HttpModule, Http } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { ChartComponent } from './util/chart/chart.component';
+import { ChartService } from './util/chart/chart.service';
+import { ChartDirective } from './util/chart/chart.directive';
+import { LineChartComponent } from './lineChart/lineChart.component';
+import { PieChartComponent } from './pieChart/pieChart.component';
+import { BarChartComponent } from './barChart/barChart.component';
+import { VersionsComponent } from './reports/versions/versions.component';
 
 const routes: Routes = [
-
+    { path: 'versions', component: VersionsComponent }
 ];
 
 @NgModule({
@@ -16,13 +23,21 @@ const routes: Routes = [
         RouterModule
     ],
     declarations: [
-
+        VersionsComponent,
+        ChartComponent,
+        ChartDirective,
+        LineChartComponent,
+        PieChartComponent,
+        BarChartComponent
     ],
     providers: [
-
+        ChartService
     ],
     exports: [
-        RouterModule
-    ]
+        RouterModule,
+        VersionsComponent,
+        ChartComponent
+    ],
+    entryComponents: [LineChartComponent, PieChartComponent, BarChartComponent]
 })
 export class AppRoutingModule { }
--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { ChartService } from './util/chart/chart.service';
-import { ChartComponent } from './util/chart/chart.component';
-import { ChartDirective } from './util/chart/chart.directive';
-import { LineChartComponent } from './lineChart/lineChart.component';
-import { PieChartComponent } from './pieChart/pieChart.component';
-import { BarChartComponent } from './barChart/barChart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -20,18 +14,9 @@ import { AppComponent } from './app.component';
     HttpModule,
   ],
   declarations: [
-    AppComponent,
-    ChartComponent,
-    LineChartComponent,
-    PieChartComponent,
-    BarChartComponent,
-    ChartDirective
+    AppComponent
   ],
-  providers: [
-    ChartService
-  ],
-  bootstrap: [AppComponent],
-
-  entryComponents: [LineChartComponent, PieChartComponent, BarChartComponent]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
