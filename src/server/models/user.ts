3cf38fc145193f71087fb558f614b4dcc7d37ae5
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-28 11:59:47 +0300

    Added user model

---
+++ src/server/models/user.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+import { IUser } from '../types/IUser';
+
+const Schema = mongoose.Schema;
+
+export interface UserDocument extends mongoose.Document, IUser {
+    _id: string,
+    createdAt: Date,
+    modifiedAt: Date
+}
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    });
+
+export var UserModel = mongoose.model<UserDocument>('User', UserSchema);
