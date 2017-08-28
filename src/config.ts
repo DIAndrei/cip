d6442d6b085e57a585e5c76cbe6095acf68f45dd
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-28 11:58:48 +0300

    Added config file

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    DB: 'mongodb://localhost/charts',
+    SECRET: 'ff555f9eb2b04b20b017a034b859bcac'
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as morgan from 'morgan';
+import { CONFIG } from './config';
 
 const port: number = process.env.PORT || 3000;
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 (<any>mongoose).Promise = global.Promise;
 (async () => {
     try {
-        await mongoose.connect("mongodb://localhost/charts");
+        await mongoose.connect(CONFIG.DB);
     } catch (err) {
         console.trace(err);
     }
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, './client')));
 app.use(express.static(path.join(__dirname, '../')));
 
 app.use(require('./server/routes/data.routes'));
+app.use(require('./server/routes/user.routes'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'));
