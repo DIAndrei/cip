b68acd9acb0bc4d347c086d49c9970a428845d71
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-28 09:41:35 +0300

    Added gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,34 @@ const sourcemaps = require('gulp-sourcemaps');
 //task to be run when the watcher detects changes
 gulp.task('compile', () => {
     const tsProject = ts.createProject('tsconfig.json');
-    const tsResult = 
-    gulp.src([
-        'src/**/*.ts',
-        '!src/client/**/*.ts'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(tsProject());
+    const tsResult =
+        gulp.src([
+            'src/**/*.ts',
+            '!src/client/**/*.ts'
+        ])
+            .pipe(sourcemaps.init())
+            .pipe(tsProject());
 
-    return tsResult.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src'}))
-    .pipe(gulp.dest('./build'));
+    return tsResult.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../src' }))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('compile:client', () => {
     const tsProject = ts.createProject('tsconfig.json');
-    const tsResult = 
-    gulp.src('src/client/**/*.ts')
-    .pipe(sourcemaps.init())
-    .pipe(tsProject());
+    const tsResult =
+        gulp.src('src/client/**/*.ts')
+            .pipe(sourcemaps.init())
+            .pipe(tsProject());
 
-    return tsResult.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src/client'}))
-    .pipe(gulp.dest('./build/client'));
+    return tsResult.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../../src/client' }))
+        .pipe(gulp.dest('./build/client'));
 });
 
 //set up a watcher to watch over changes
-gulp.task('watch', ['scripts'], () => {
-    gulp.watch('**/*.ts', ['scripts']);
+gulp.task('watch', ['compile', 'compile:client', 'copy'], () => {
+    gulp.watch('./src/server/**/*.ts', ['compile']);
+    gulp.watch('./src/client/**/*.ts', ['compile:client']);
+    gulp.watch('./src/client/**/*.+(html|js|css)', ['copy']);
 });
 
 gulp.task('copy', () => {
