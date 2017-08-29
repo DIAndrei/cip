47cd4f250a2c9c5b95025e6db3b3b3fe9bd4ab0e
Andreea Dumitru <dumitru_andreea96@yahoo.com>
2017-08-29 14:14:12 +0300

    create profile component

---
+++ src/client/user/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProfileService } from './profile.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'profile',
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent  { 
+
+  
+
+}
---
+++ src/client/user/profile/profile.service.ts
@@ -0,0 +1,12 @@
+import { Inject, Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProfileService {
+    
+}
