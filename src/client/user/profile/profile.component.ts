ef78f7ee7e44925ac4f18d8b5eace8b3c780f57c
Andrei Diaconu <andrei_di@yahoo.com>
2017-08-29 14:57:35 +0300

    Using session service to get the current user

--- src/client/user/profile/profile.component.ts
+++ src/client/user/profile/profile.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProfileService } from './profile.service';
+import { SessionService } from '../../util/session.service';
 
 @Component({
   moduleId: module.id,
   selector: 'profile',
   templateUrl: './profile.component.html'
 })
-export class ProfileComponent  { 
+export class ProfileComponent {
+  constructor(private _sessionService: SessionService) { }
 
-  
-
+  getUser() {
+    return this._sessionService.profile.email;
+  }
 }
